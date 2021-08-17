29832b79b756b1fcec53203bd1783ec39b2407e7
Tselate <Tselatedawit@outlook.com>
2021-08-17 12:17:33 -0600

    Css

---
+++ index.js
@@ -0,0 +1,14 @@
+const turnDisplay = document.querySelector(".turnDisplay")
+const player1ScoreBoard = document.querySelector(".player1scoredBoard")
+const player2ScoreBoard = document.querySelector(".player2scoredBoard")
+const player1Dice = document.querySelector(".player1Dice")
+const player2Dice = document.querySelector(".player2Dice")
+const diceGif = document.querySelector(".image")
+const playMessage = document.querySelector(".play")
+const rollDiceBtn = document.querySelector(".rollDice")
+const resetBtn = document.querySelector(".reset")
+
+
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
